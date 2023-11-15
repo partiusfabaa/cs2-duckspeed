2c8d7d70fbc8f50a3422be17f9370b0d02f8d005
hyo <96542489+partiusfabaa@users.noreply.github.com>
2023-11-15 17:14:32 +0500

    1

---
+++ duckspeed/DuckSpeed.cs
@@ -0,0 +1,32 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Timers;
+
+namespace DuckSpeed;
+
+public class DuckSpeed : BasePlugin
+{
+    public override string ModuleAuthor => "thesamefabius";
+    public override string ModuleName => "Duck Speed";
+    public override string ModuleVersion => "v1.0.0";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnTick>(() =>
+        {
+            foreach (var player in Utilities.GetPlayers()
+                         .Where(player => player is { IsBot: false, IsValid: true, PawnIsAlive: true }))
+            {
+                if (player.Buttons.HasFlag(PlayerButtons.Duck))
+                {
+                    var movementService = player.PlayerPawn.Value.MovementServices;
+                    if (movementService != null)
+                    {
+                        //6.023437f
+                        new CCSPlayer_MovementServices(movementService.Handle).DuckSpeed = 6.023437f;
+                    }
+                }
+            }
+        });
+    }
+}
